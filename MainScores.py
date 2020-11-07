df548cb3a9983c150d7a29d58f80bb410bda4f71
bella738 <70857289+bella738@users.noreply.github.com>
2020-11-07 22:15:18 +0200

    Update MainScores.py

--- MainScores.py
+++ MainScores.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def score_server():
     try:
-        score = open("/Users/bellamarkovitz/Documents/devops/project 1/Score.txt", "r")
+        score = open("./Score.txt", "r")
     except BaseException as e:
         return """<html>
         <head>
